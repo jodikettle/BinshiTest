49a7ab3eccead48634817a60955d4d1c47353e5c
Jodi Kettle <jodi.kettle@gmail.com>
2017-01-16 22:10:06 +0000

    desktop view

---
+++ FrontPage/Scripts/header.js
@@ -0,0 +1,34 @@
+﻿function init() {
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 80
+            container = $("header"),
+            header = $(".navbar"),
+            logo = $(".navbar-brand")
+            jumbo = $(".jumbotron");
+
+
+        console.log("Scrolling " + distanceY);
+
+        if (distanceY >= shrinkOn) {
+            jumbo.addClass("hideJumbo");
+            logo.addClass("showLogo");
+            container.addClass("smallHeader");
+            header.addClass("smaller");
+        } else {
+            if (jumbo.hasClass("hideJumbo")) {
+                jumbo.removeClass("hideJumbo");
+            }
+            if (logo.hasClass("showLogo")) {
+                logo.removeClass("showLogo");
+            }
+            if (container.hasClass("smallHeader")) {
+                container.removeClass("smallHeader");
+            }
+            if (header.hasClass("smaller")) {
+                header.removeClass("smaller");
+            }
+        }
+    });
+}
+window.onload = init();
