d878c7c6fc4ba92d35b224baf570635180477de8
Jodi Kettle <jodi.kettle@gmail.com>
2017-01-19 19:24:00 +0000

    update desktop and mobile view

--- FrontPage/Scripts/header.js
+++ FrontPage/Scripts/header.js
@@ -5,30 +5,46 @@
             container = $("header"),
             header = $(".navbar"),
             logo = $(".navbar-brand")
-            jumbo = $(".jumbotron");
+            jumbo = $(".jumbotron")
+            shares = $(".shares-box");
+            var mobileScroll = 80;
+
+            var width = $(window).width();
 
 
-        console.log("Scrolling " + distanceY);
+         console.log("Scrolling " + distanceY);
 
-        if (distanceY >= shrinkOn) {
-            jumbo.addClass("hideJumbo");
-            logo.addClass("showLogo");
-            container.addClass("smallHeader");
-            header.addClass("smaller");
-        } else {
-            if (jumbo.hasClass("hideJumbo")) {
-                jumbo.removeClass("hideJumbo");
-            }
-            if (logo.hasClass("showLogo")) {
-                logo.removeClass("showLogo");
-            }
-            if (container.hasClass("smallHeader")) {
-                container.removeClass("smallHeader");
-            }
-            if (header.hasClass("smaller")) {
-                header.removeClass("smaller");
-            }
-        }
+         if (width > 768) {
+             if (distanceY >= shrinkOn) {
+                 jumbo.addClass("hideJumbo");
+                 logo.addClass("showLogo");
+                 container.addClass("smallHeader");
+                 header.addClass("smaller");
+             } else {
+                 if (jumbo.hasClass("hideJumbo")) {
+                     jumbo.removeClass("hideJumbo");
+                 }
+                 if (logo.hasClass("showLogo")) {
+                     logo.removeClass("showLogo");
+                 }
+                 if (container.hasClass("smallHeader")) {
+                     container.removeClass("smallHeader");
+                 }
+                 if (header.hasClass("smaller")) {
+                     header.removeClass("smaller");
+                 }
+             }
+         }
+         else {
+             if (distanceY >= mobileScroll) {
+                 shares.addClass('hide');
+             }
+             else {
+                 if (shares.hasClass("hide")) {
+                     shares.removeClass('hide');
+                 }
+             }
+         }
     });
 }
 window.onload = init();
